b1613d6e41924a732181510699ae1e7859a734c5
Lennart Jongeneel <lennart@1200wd.com>
2017-06-04 21:18:32 +0200

    [ADD] Install flex theme with setup shell script

--- setup.sh
+++ setup.sh
@@ -5,3 +5,8 @@ virtualenv ~/.virtualenvs/web1200
 
 # Install required python modules
 ~/.virtualenvs/web1200/bin/pip install pelican markdown
+
+mkdir themes
+cd themes
+
+git clone https://github.com/alexandrevicenzi/Flex.git
