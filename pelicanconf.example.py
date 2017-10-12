3b1ab18f810291a69d619afec618fdaf089d18e3
Lennart Jongeneel <lennart@1200wd.com>
2017-10-12 17:56:41 +0200

    [REF] Use local config file

---
+++ pelicanconf.example.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+from __future__ import unicode_literals
+import socket
+
+AUTHOR = u'Lennart Jongeneel'
+SITENAME = u'Coineva'
+
+if socket.gethostname() == 'talisman':
+    SITEURL = 'http://localhost:8000'
+else:
+    SITEURL = 'http://coineva.com'
+SITETITLE = 'Coineva'
+SITESUBTITLE = 'Cryptocurrency Code Factory'
+SITEDESCRIPTION = 'bitcoin stuff'
+SITELOGO = SITEURL + '/images/Peter-Behrens-Alphabet-1908-C.png'
+# FAVICON = SITEURL + '/images/favicon.ico'
+PYGMENTS_STYLE = 'monokai'
+
+ROBOTS = 'index, follow'
+PATH = 'content'
+
+TIMEZONE = 'Europe/Amsterdam'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+USE_FOLDER_AS_CATEGORY = True
+MAIN_MENU = True
+HOME_HIDE_TAGS = True
+DISPLAY_PAGES_ON_MENU = False
+
+# Blogroll
+LINKS = (('What to do with my paper wallet?', '/what-to-do-with-my-paper-wallet.html'),
+         # ('5 ways to explain bitcoin', '/five-ways-to-explain-bitcoin.html'),
+         # ('why bitcoin?', '/why-bitcoin.html'),
+         ('bitcoinlib', '/python-bitcoin-library.html'),
+         ('bulkpaperwallets', 'https://github.com/1200wd/bulkpaperwallets'))
+
+# MENUITEMS = (('Archives', '/archives.html'),
+#              ('Categories', '/categories.html'),
+#              ('Tags', '/tags.html'),)
+
+# Social widget
+SOCIAL = (('linkedin', 'https://www.linkedin.com/company-beta/8098420/'),
+          ('github', 'https://github.com/1200wd'),
+          ('twitter', 'https://twitter.com/1200wd'),
+          ('rss', 'feeds/all.atom.xml'))
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+THEME = "themes/pelican-themes-tuxlite-tbs"
+SITESUBTITLES = ('Coineva', 'cryptocurrency code factory')
+
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = [u"disqus_static"]
+
+DISQUS_SITENAME = u'coineva'
+DISQUS_SECRET_KEY = u'qkcQOndFztjC6k7SxYYWiOEBrKlJYFnaeIOCmYX7wN8a9rzl5KBndSycEHCSLLOv'
+DISQUS_PUBLIC_KEY = u'qdXg9adNv1duVH8hS3UY7193ytG4JSHaiEQOmhaReZcxvCxoFDCayP9lHBfWg5sn'
